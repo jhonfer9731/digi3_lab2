ed95a1ade8da948b116bcfc9cc98626541dddc5b
Jhon Benavides <jfernando.benavides@udea.edu.co>
2020-11-19 16:40:12 -0500

    Se agrega la funcionalidad de esperar 10 segundos y poner la banda en suspension si no pasan botellas

--- main.c
+++ main.c
@@ -21,22 +21,22 @@ void Timer1_init(void){
        and SC_channel register
     */
 
-   // clock initialisation
+   // clock initialisation ( bus rate 8MHz )
 
 	TPM1SC_CLKSB=0;
 	TPM1SC_CLKSA=0;
-   TPM1SC_TOIE = 1; //timer overflow input enable (enables the output interrupt request)
-   TPM1SC_CPWMS = 0; // disabled the edge-aligned PWM
-
-   TPM1SC_PS = 7; // configure the pre-scale
-
-   TPM1MOD = MODULO_TOPE_TIMER; // configure the module register of timer (16bits)
-
-
-   TPM2SC_TOIE =1; //timer overflow input enable (enables the output interrupt request)
-   TPM2SC_PS = 7; // configure the pre-scale
-   TPM2SC_CPWMS = 0;
-   TPM2MOD = 65000;
+	TPM1SC_TOIE = 1; //timer overflow input enable (enables the output interrupt request)
+	TPM1SC_CPWMS = 0; // disabled the edge-aligned PWM
+	
+	TPM1SC_PS = 5; // configure the pre-scale
+	
+	TPM1MOD = MODULO_TOPE_TIMER; // configure the module register of timer (16bits)
+	
+	
+	TPM2SC_TOIE =1; //timer overflow input enable (enables the output interrupt request)
+	TPM2SC_PS = 7; // configure the pre-scale
+	TPM2SC_CPWMS = 0;
+	TPM2MOD = 0;
    
    
    
@@ -45,7 +45,10 @@ void Timer1_init(void){
    
    
    
-    /* **** Configuration of timer channels **** 0 */
+    /* **** Configuration of timer channels ****  */
+   
+   /* Configuration of timer channel 0 */
+   /* in charge of the bottle sensor */
    
     TPM1C0SC_CH0IE = 1; // interrupt enable
 
@@ -59,7 +62,7 @@ void Timer1_init(void){
     
     
     /* Configuration of timer channel 1 */
-    /* manages the on/off button */
+    /* manages the on/off button  (switch) */
     
     TPM1C1SC_CH1IE = 1; // start/stop pin
     
@@ -72,68 +75,73 @@ void Timer1_init(void){
 	
 	
 	/* Configuration of timer channel 2 */
-	    /* manages the Borra_cuentas button */
-	    
-	    TPM1C2SC_CH2IE = 1; // start/pin
-	    
-		// select (Input Capture) Timer Mode
-		TPM1C2SC_MS2A = 0;
-		TPM1C2SC_MS2B = 0;
-		// capture on rising edge only
-		TPM1C2SC_ELS2A = 1;
-		TPM1C2SC_ELS2B = 0;
+	/* manages the Borra_cuentas button */
+	
+	TPM1C2SC_CH2IE = 1; // start/pin
+	
+	// select (Input Capture) Timer Mode
+	TPM1C2SC_MS2A = 0;
+	TPM1C2SC_MS2B = 0;
+	// capture on falling edge only
+	TPM1C2SC_ELS2A = 0;
+	TPM1C2SC_ELS2B = 1;
 	
 	/* Configuration of timer channel 3 */
-		/* manages the Cuenta_Standard button */
-		
-		TPM1C3SC_CH3IE = 1; // Cuenta_Standard
-		
-		// select (Input Capture) Timer Mode
-		TPM1C3SC_MS3A = 0;
-		TPM1C3SC_MS3B = 0;
-		// capture on rising edge only
-		TPM1C3SC_ELS3A = 1;
-		TPM1C3SC_ELS3B = 0;
+	/* manages the Cuenta_Standard button */
+	
+	TPM1C3SC_CH3IE = 1; // Cuenta_Standard
+	
+	// select (Input Capture) Timer Mode
+	TPM1C3SC_MS3A = 0;
+	TPM1C3SC_MS3B = 0;
+	// capture on falling edge only
+	TPM1C3SC_ELS3A = 0;
+	TPM1C3SC_ELS3B = 1;
 		
 	/* Configuration of timer channel 4 */
-		/* manages the Cuenta_stand_mas button */
-		
-		TPM1C4SC_CH4IE = 1; // Cuenta_stand_mas
-		
-		// select (Input Capture) Timer Mode
-		TPM1C4SC_MS4A = 0;
-		TPM1C4SC_MS4B = 0;
-		// capture on rising edge only
-		TPM1C4SC_ELS4A = 1;
-		TPM1C4SC_ELS4B = 0;
+	/* manages the Cuenta_stand_mas button */
+	
+	TPM1C4SC_CH4IE = 1; // Cuenta_stand_mas
+	
+	// select (Input Capture) Timer Mode
+	TPM1C4SC_MS4A = 0;
+	TPM1C4SC_MS4B = 0;
+	// capture on rising edge only
+	TPM1C4SC_ELS4A = 0;
+	TPM1C4SC_ELS4B = 1;
+	
+	
 	/* Configuration of timer channel 5 */
-		/* manages the Cuenta_stand_menos button */
-		
-		TPM1C5SC_CH5IE = 1; // Cuenta_stand_menos
-		
-		// select (Input Capture) Timer Mode
-		TPM1C5SC_MS5A = 0;
-		TPM1C5SC_MS5B = 0;
-		// capture on rising edge only
-		TPM1C5SC_ELS5A = 1;
-		TPM1C5SC_ELS5B = 0;
+	/* manages the Cuenta_stand_menos button */
+	
+	TPM1C5SC_CH5IE = 1; // Cuenta_stand_menos
+	
+	// select (Input Capture) Timer Mode
+	TPM1C5SC_MS5A = 0;
+	TPM1C5SC_MS5B = 0;
+	// capture on falling edge only
+	TPM1C5SC_ELS5A = 0;
+	TPM1C5SC_ELS5B = 1;
+	
+	
 	/* Configuration of timer channel 6 */
-		/* manages the Average_Defectuosas button */
-		
-		TPM2C0SC_CH0IE = 1; // Average_Defectuosas
-		
-		// select (Input Capture) Timer Mode
-		TPM2C0SC_MS0A = 0;
-		TPM2C0SC_MS0B = 0;
-		// capture on rising edge only
-		TPM2C0SC_ELS0A = 1;
-		TPM2C0SC_ELS0B = 0;
+	/* manages the Average_Defectuosas button */
+	
+	TPM2C0SC_CH0IE = 1; // Average_Defectuosas
+	
+	// select (Input Capture) Timer Mode
+	TPM2C0SC_MS0A = 0;
+	TPM2C0SC_MS0B = 0;
+	// capture on falling edge only
+	TPM2C0SC_ELS0A = 0;
+	TPM2C0SC_ELS0B = 1;
+	
+	
 
-    
-
-    //activate the bus clock A
+    /*** activate the bus clocks ****/
+	
     TPM1SC_CLKSA = 1;
-    TPM1SC_CLKSB=0;// BUS RATE CLOCK
+    TPM1SC_CLKSB = 0;// BUS RATE CLOCK
     
     
     TPM2SC_CLKSA = 1;
@@ -147,11 +155,11 @@ void Port_Init( void ){
 	
 	
     PTADD = 0xFF; // output port
-    PTAD = 0x00; // clear port
+    PTAD = 0x00; // clear port (actuators)
     //PTEDD = 0x00;
-    PTED = 0x00;
+    //PTED = 0x00;
     //PTFDD = 0x00;
-    PTFD = 0x00;
+    //PTFD = 0x00;
     
     /*  PTAD[2] boton start/stop */
 
@@ -167,7 +175,7 @@ interrupt 9 void TPM1InputCapture_ISR(void){
     */
    // clear the interrupt request of CHOF (channel 0 flag)
    
-   TPM1C0SC;//1 step, read the ch0 SC register
+   TPM1C0SC; //1 step, read the ch0 SC register
    TPM1C0SC_CH0F = 0; // 2 step, clear CH0F
    flag_inputCapture = 1; // indicates the main program the input capture
 
@@ -189,13 +197,12 @@ interrupt 10 void TPM1OnOFFButton_ISR(void){
 interrupt 11 void borrarCuentas_ISR(void){
 	/* attends the interrupt request of timer when 
 		there is a  rising edge
-
-		It happens when the Borra_cuentas button is pressed
-		*/
-		// clear the interrupt request of CHOF (channel 1 flag)
-		TPM1C2SC;//1 step, read the ch1 SC register
-		TPM1C2SC_CH2F = 0; // 2 step, clear CH1F
-		flag_inputCapture = 3;// indicates the main program the input capture with Borra_Cuentas
+	It happens when the Borra_cuentas button is pressed
+	*/
+	// clear the interrupt request of CHOF (channel 1 flag)
+	TPM1C2SC;//1 step, read the ch1 SC register
+	TPM1C2SC_CH2F = 0; // 2 step, clear CH1F
+	flag_inputCapture = 3;// indicates the main program the input capture with Borra_Cuentas
 		
 }
 
@@ -250,9 +257,6 @@ interrupt 16 void averageDefectuosas_ISR(void){
 
 
 
-
-
-
 interrupt 15 void TPM1_Overflow_ISR(void){
 
     /* attends the interrupt request of timer when 
@@ -272,7 +276,6 @@ void main(void){
 
     char firstEdge_flag = 0; // indicates the fist and second edge of the IC interrupt
     unsigned char movimiento = 0; // initial state of  conveyor belt
-    
     unsigned int numeroBotellas = 0;
     unsigned char defecto = 0; // flag for wrong bottles
 	unsigned int numeroBotellasStd = 0;
@@ -280,10 +283,13 @@ void main(void){
     unsigned int cuentaStandardMenos = 0;
     unsigned int averageDefectuosas  = 0;
     unsigned int botellasDefectuosas  = 0;
-    unsigned int variableSeleccion = 0;
-    unsigned int min = 5000; //Min & Max to define the size range for Standard bottles.
-	unsigned int max = 6000;
-	unsigned int tmax = 0;
+    unsigned int variableSeleccion = 0;  // it has the number of one bottle category
+    unsigned int min = 5000; // Min & Max to define the size range for Standard bottles.
+	unsigned int max = 7000; 
+	unsigned int tmax = 0; //Counter for the 10 seconds
+	char mensajeApagado[] = "Tiempo de espera maximo alcanzado, el sistema se apagara ....";
+	
+	
     // system initialisation
     SOPT1_COPT  = 0; // disable the WatchDog
     Timer1_init();
@@ -323,9 +329,9 @@ void main(void){
             // verify if it is first or second edge
             if(firstEdge_flag == 0 && movimiento){
                 overflow_Count = 0; // reset the overflow counter
-                firstEdge_flag = 1;
+                firstEdge_flag = 1; 
                 time_edge_1 = TPM1C0V; // save the current count of the timer
-                PTAD_PTAD0 = 1; // the bottle just got captured by sensor
+                PTAD_PTAD0 = 1; // the bottle just got captured by sensor (LED)
                 PTAD_PTAD1 = 0;
             }
             else if(firstEdge_flag == 1 && movimiento){
@@ -333,7 +339,7 @@ void main(void){
                 firstEdge_flag = 0; 
                 time_edge_2 = TPM1C0V; // save the current count of the timer
                 PTAD_PTAD0 = 0;
-                PTAD_PTAD1 = 1; // the bottle just left the sensor
+                PTAD_PTAD1 = 1; // the bottle just left the sensor (LED)
                 time_pulse_width = (long) (time_edge_2 + overflow_Count*MODULO_TOPE_TIMER - time_edge_1); // calculates the time the bottle takes to go across the sensor
                 if (time_pulse_width>=min && time_pulse_width<=max){
 					numeroBotellasStd++;
@@ -350,10 +356,11 @@ void main(void){
 					defecto=1;
 				}
 				else{
+					/* it never goes into */
 					botellasDefectuosas++;
 					defecto=1;
 				}
-				PTAD_PTAD4 = defecto;
+				PTAD_PTAD4 = defecto; /* LED is active */ //PTAD[4] indicates the non-standard status
                 overflow_Count = 0; // reset the overflow counter
                 numeroBotellas++; // increases the bottle counter
 
@@ -370,6 +377,10 @@ void main(void){
         		// off state
 				PTAD_PTAD2 = 0; // PTAD[2] stops the conveyor belt
 				movimiento = 0;
+				PTAD_PTAD0 = 0;
+				PTAD_PTAD1 = 0; // the bottle just left the sensor (LED)
+				defecto = 0;
+				PTAD_PTAD4 = 0;
         	}
         	PTAD_PTAD3 = movimiento; //PTAD[3] indicates the movement status
         	
@@ -378,7 +389,7 @@ void main(void){
         // it indicates that Borrar_cuentas button was pressed
         else if(flag_inputCapture == 3){
         	flag_inputCapture = 0;
-        	if(!movimiento){
+        	if(!movimiento){ 
         		numeroBotellas = 0; // the number of bottles is reset
         	}
         }
@@ -406,7 +417,16 @@ void main(void){
 				variableSeleccion = 0;
 			}
         }
-        
+        else if(overflow_Count > 2){ // Enter when 
+        	overflow_Count = 0;
+        	PTAD_PTAD2 = 0; // PTAD[2] stops the conveyor belt
+        	movimiento = 0;
+        	PTAD_PTAD3 = movimiento; //PTAD[3] indicates the movement status
+        	PTAD_PTAD0 = 0;
+			PTAD_PTAD1 = 0;
+			defecto = 0;
+			PTAD_PTAD4 = 0;
+        }
     }
 
 
